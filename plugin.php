101977723044d9a7e2065abf607b93d06707e043
Rosano Coutinho <pub@xeari.com>
2021-08-25 11:31:53 -0500

    Define list action

--- plugin.php
+++ plugin.php
@@ -1,50 +1,21 @@
 <?php
 /*
-Plugin Name: Custom API action
-Plugin URI: http://yourls.org/
-Description: Define custom API action 'delete'
-Version: 1.0
-Author: Ozh
-Author URI: http://ozh.org/
+Plugin Name: Custom API action (list)
+Plugin URI: http://yourls.org
+Description: Define custom API action 'list'
+Version: 0.1
+Author: rosano.ca
+Author URI: https://rosano.ca
 */
 
-// Define custom action "delete"
-yourls_add_filter( 'api_action_delete', 'my_delete_function' );
+// Define custom action "list"
+yourls_add_filter( 'api_action_list', 'my_list_function' );
 
-// Actually delete
-function my_delete_function() {
-	// Need 'shorturl' parameter
-	if( !isset( $_REQUEST['shorturl'] ) ) {
-		return array(
-			'statusCode' => 400,
-			'simple'     => "Need a 'shorturl' parameter",
-			'message'    => 'error: missing param',
-		);	
-	}
-	
-	$shorturl = $_REQUEST['shorturl'];
-
-	// Check if valid shorturl
-	if( !yourls_is_shorturl( $shorturl ) ) {
-		return array(
-			'statusCode' => 404,
-			'simple '    => 'Error: short URL not found',
-			'message'    => 'error: not found',
-		);	
-	}
-	
-	// Delete shorturl
-	if( yourls_delete_link_by_keyword( $shorturl ) ) {
-		return array(
-			'statusCode' => 200,
-			'simple'     => "Shorturl $shorturl deleted",
-			'message'    => 'success: deleted',
-		);	
-	} else {
-		return array(
-			'statusCode' => 500,
-			'simple'     => 'Error: could not delete shorturl, not sure why :-/',
-			'message'    => 'error: unknown error',
-		);	
-	}
+// Actually list
+function my_list_function() {
+	return array(
+		'statusCode' => 200,
+		'message'    => 'success: listed',
+		'result' => yourls_get_db()->fetchAll("SELECT keyword FROM yourls_url"),
+	);	
 }
