3bccad64e341a75229ba1d02b31fdadbd9f64c4c
LeoX DEV <84378319+Le0X8@users.noreply.github.com>
2023-09-11 10:35:06 +0200

    Added URL return

--- plugin.php
+++ plugin.php
@@ -3,9 +3,9 @@
 Plugin Name: Custom API action (list)
 Plugin URI: http://yourls.org
 Description: Define custom API action 'list'
-Version: 0.1
-Author: rosano.ca
-Author URI: https://rosano.ca
+Version: 0.2
+Author: LeoX DEV
+Author URI: https://leox.dev
 */
 
 // Define custom action "list"
@@ -17,6 +17,6 @@ function my_list_function() {
 	return array(
 		'statusCode' => 200,
 		'message'    => 'success',
-		'result' => yourls_get_db()->fetchAll("SELECT keyword FROM `$table`"),
+		'result' => yourls_get_db()->fetchAll("SELECT keyword, url FROM `$table`"),
 	);	
 }
